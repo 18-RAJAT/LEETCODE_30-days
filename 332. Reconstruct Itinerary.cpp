0f8a62f27d6745cf2ba3b8aeecb4e9eb21a76247
Rajat_Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-01-16 18:45:15 +0530

    Add files via upload

---
+++ 332. Reconstruct Itinerary.cpp
@@ -0,0 +1,35 @@
+class Solution {    
+
+    void dfs(unordered_map<string,multiset<string>>& adj,vector<string>& result,string s)
+    {
+        while(adj[s].size())
+        {
+            string v = *(adj[s].begin())
+            adj[s].erase(adj[s].begin())
+
+            dfs(adj,result,v);    
+            
+        }
+        
+        result.push_back(s);
+    }
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        
+        //build the adj list
+        
+        unordered_map<string,multiset<string>> adj;
+        
+        for(vector<string>& t : tickets)
+        {
+            adj[t[0]].insert (t[1]);    }
+             vector<string>result;
+        
+        dfs(adj,result,"JFK");
+        
+        reverse(result.begin(),result.end());
+        
+        return result;
+    }
+    
+};
