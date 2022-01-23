41a75830d383bdc92fb91dab4d9870ee3595818c
Rajat_Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-01-23 15:23:00 +0530

    Add files via upload

---
+++ 1291. Sequential Digits.cpp
@@ -0,0 +1,34 @@
+//Using dfs + Recursion
+
+class Solution {
+public:
+    
+    vector<int>ans;
+    
+    void dfs(int low,int high,int i,int num)
+    {
+        //Lies in the range
+        if(num >= low && num <=high)
+        {
+          ans.push_back(num);
+        }
+        
+        if(num > high || i > 9)
+        {
+            return;
+        }
+        
+        dfs(low,high,i+1,num*10 +i);
+    }
+    vector<int> sequentialDigits(int low, int high) {
+        
+        for(int i=1;i<=9;++i)
+        {
+            dfs(low,high,i,0);
+
+            sort (ans.begin(),ans.end());
+        }
+        
+        return ans;
+    }
+};
