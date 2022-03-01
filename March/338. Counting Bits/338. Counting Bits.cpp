39c65645b94a2a5bab322c4fe991a959583340f9
Rajat_Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-03-01 10:31:40 +0530

    Add files via upload

---
+++ March/338. Counting Bits/338. Counting Bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        
+        vector<int>res(n+1,0);
+        
+        for(int i=1;i<=n;++i)
+        {
+            res[i] = res[i & (i-1)] + 1;
+        
+        }
+        return res;
+    }
+};
+
